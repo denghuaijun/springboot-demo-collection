b9002a628e2e7a3ead2654c54be0714713caaa4c
denghuaijun <dhjtobenumber1@163.com>
2020-12-28 14:42:08 +0800

    图数据库模块添加

--- springboot-demo-neo4j/src/main/java/com/dhj/demo/neo4j/config/Neo4jConfig.java
+++ springboot-demo-neo4j/src/main/java/com/dhj/demo/neo4j/config/Neo4jConfig.java
@@ -1,8 +1,12 @@
 package com.dhj.demo.neo4j.config;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
 import org.neo4j.ogm.config.ClasspathConfigurationSource;
 import org.neo4j.ogm.config.ConfigurationSource;
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
@@ -12,6 +16,15 @@ import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
  */
 @Configuration
 public class Neo4jConfig {
+
+    @Value("${spring.data.neo4j.uri}")
+    private String uri;
+
+    @Value("${spring.data.neo4j.username}")
+    private String username;
+
+    @Value("${spring.data.neo4j.password}")
+    private String password;
 
     @Bean
     public SessionFactory sessionFactory(){
@@ -27,6 +40,15 @@ public class Neo4jConfig {
     @Bean
     public Neo4jTransactionManager transactionManager(){
         return new Neo4jTransactionManager(sessionFactory());
+    }
+    /**
+     * 图数据库驱动模式
+     * @return
+     */
+
+    @Bean
+    public Driver neo4jDriver() {
+        return GraphDatabase.driver(uri, AuthTokens.basic(username,password));
     }
 
 }
